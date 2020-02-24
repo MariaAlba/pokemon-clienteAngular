0f4fdf7ac016266a9cecc617aad418d84a8a4fc0
Maria <Curso@IPARTEKAULA.COM>
2020-02-24 12:25:42 +0100

    CRUD funcional

--- src/app/model/pokemon.ts
+++ src/app/model/pokemon.ts
@@ -2,11 +2,13 @@ export class Pokemon {
 
     id: number;
     nombre: string;
+    imagen:string;
     habilidades: Array<any>;
 
     constructor() {
         this.id = 0;
         this.nombre = '';
+        this.imagen = 'https://f0.pngfuel.com/png/295/911/pokemon-pokeball-png-clip-art.png';
         this.habilidades = [];
     }
 
